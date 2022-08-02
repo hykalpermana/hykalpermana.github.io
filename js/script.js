b7c01dc54d808ab0a9fea7a1cf45ce06a0b07241
hykalpermana <haikalpermana54@gmail.com>
2022-08-03 06:18:21 +0700

    zero

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-document.getElementById("submit").onclick = function () {
-    var tebakan = document.getElementById("tebakan").value;
-    var jawaban = "bapa bapa";
-
-    if (tebakan == jawaban) {
-        alert("anjay benar")
-    } else {
-        alert("yahaha salah")
-    }
-}
-
-function klik() {
-    alert('Selamat anda telah menjadi tim sukses partai tersebut.');
-}
-
-
 document.getElementById("pdip").onclick = function () {
     document.getElementById("gambarpartai").src = "gambar/gambarpdi.jpg";
 }
@@ -26,11 +10,32 @@ document.getElementById("gerindra").onclick = function () {
     document.getElementById("gambarpartai").src = "gambar/gambargerindra.jpeg";
 }
 
-document.getElementById("refresh").onclick = function () {
-    document.getElementById("gambarpartai").src = "";
+document.getElementById("refresh").onclick = function (){
+    document.getElementById("gambarpartai").src="";
+}
+
+document.getElementById("pilih").onclick = function (){
+    alert("Selamat anda telah menjadi tim sukses partai tersebut!")
 }
 
 document.getElementById("tampil").onclick = function () {
     var a = document.getElementById("text").value;
     alert(a);
+}
+
+document.getElementById("submit").onclick = function () {
+    var tebakan = document.getElementById("tebakan").value;
+    var jawaban = "bapa bapa";
+
+    if (tebakan == jawaban) {
+        alert("anjay benar");
+    }
+    else if (tebakan == "") {
+        alert("Anda belom menjawab");
+      }
+    
+    else {
+        alert("yahaha salah");
+    }
+    
 }
